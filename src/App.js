ba1dee84546452637a66f84c9063c04ed84d9fab
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-04-24 09:10:15 -0300

    Hooks vs. Connect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
 import { Col } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { getPokemon } from './api';
 import logo from './statics/logo.svg';
 import './App.css';
-import { setPokemons as setPokemonsActions } from './actions';
+import { setPokemons } from './actions';
 
-function App({ pokemons, setPokemons }) {
-  console.log('🚀 ~ App ~ pokemons:', pokemons);
+function App() {
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes);
+      dispatch(setPokemons(pokemonsRes));
     };
+
     fetchPokemons();
   }, []);
 
@@ -31,12 +34,4 @@ function App({ pokemons, setPokemons }) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
